28a0fb4e0663a8df3abac90b108682652a928205
CarmelBar <carmelbar3@gmail.com>
2016-11-26 20:57:03 +0200

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,38 @@
+symbol_table={}
+
+
+
+
+
+def initialize():
+    return
+
+def main(file_name):
+    return
+
+def label_proccessor(file_name):
+    return
+
+def var_proccessor(file_name):
+    return
+
+def parser():
+    return
+
+def codeAcode(Acodeline):
+    Acodeline = int(Acodeline) #Acodeline as an int
+    binary_rep = '{0:16b}'.format(Acodeline)
+    binary_rep = str(binary_rep)
+    return (binary_rep.replace(" ", "0"))
+
+def codeCcode(Ccodeline):
+    return
+
+def codeJumpInstruction():
+    return
+
+def codeDestInstruction():
+    return
+
+def codeCompInstruction():
+    return
